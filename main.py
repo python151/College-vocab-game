f965e3d905e5920c3bd2959a75422d3d8ef55bc2
Matthew Miller <dogm646@gmail.com>
2019-11-28 23:49:32 +0000

    ffff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 
 import database
 import security
@@ -9,6 +9,30 @@ from sqlalchemy import create_engine, and_, or_
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import SingletonThreadPool
+
+class Player:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+        self.questionNumber = 1
+        self.points = 100
+
+class questions:
+    def get(qnumber):
+        allQuestions = open('questions.txt', 'r')
+        questionsInFormat = allQuestions.read().split("\n")
+        for q in questionsInFormat:
+            if qnumber == q.split("|")[1]:
+                return q
+    def awnser(number, awnser):
+        allQuestions = open('questionAwnsers.txt', 'r')
+        questionsInFormat = allQuestions.read().split("\n")
+        for q in questionsInFormat:
+            if number == q.split("|")[1]:
+                if awnser == q.split("|")[0]:
+                    return True
+                else:
+                    return False
 
 app = Flask(__name__)
 
@@ -35,6 +59,57 @@ def log():
 def index():
     return render_template('index.html')
 
+@app.route('/howtoplay')
+def learn():
+    return render_template("rules.html")
+
+@app.route('/game')
+def game():
+    session['score'] = 100
+    session['turn'] = 1
+    return render_template('game.html')
+
+@app.route("/update_score/")
+def update_score():
+    increment = request.form.get('increment')
+    session['score'] += increment
+    return 'success'
+
+@app.route('/restart')
+def restart():
+    session[''] = ''
+    return redirect("/game")
+
+@app.route('/readyupplayer/', methods=['POST'])
+def readyup():
+    number = request.form.get("playerNumber")
+    name = request.form.get("playerName")
+    session['player'+str(number)] = [name, number, 1, 100]
+    return 'success'
+
+@app.route('/awnser/', methods=['POST'])
+def awnser():
+    if request.method == 'POST':
+        awnserSubmited = request.form.get("awnser")
+        player = request.form.get("player")
+        qNumber = session.get("player"+player)[2]
+        playerObj = session.get("player"+str(player))
+        if questions.awnser(qNumber, awnserSubmited):
+            playerObj.points[3] += 10
+            playerObj[2] += 1
+            return questions.get(playerObj.questionNumber)
+        else:
+            playerObj[3] -= 10
+        return 'incorrect'
+
+@app.route('/points/', methods=['POST'])
+def checkPoints():
+    if request.method == 'POST':
+        return str(session.get("player"+str(request.form.get("player")))[3])
+
+
+
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template('error.html', e=e)
@@ -44,5 +119,5 @@ def internal_server(e):
     return render_template('error.html', e=e)
 
 if __name__ == '__main__':
-    app.secret_key = random.randint(1, 100000)
+    app.secret_key = bytes(random.randint(1, 100000))
     app.run(debug=True, port=3000)
